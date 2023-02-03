df1c67c6839f23cdd16f583a7ed957cb2756db45
eugene <sichi331@gmail.com>
2023-02-03 11:59:39 +0200

    Added core web api project with empty ToDoController

---
+++ ToDoList/ToDoList/src/ToDoList.Api/ToDoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoList.src.ToDoList.Api
+{
+    public class ToDoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
