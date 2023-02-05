0c1dfbbf20950be4f15f2e07c6e54189d92d2a06
eugene <sichi331@gmail.com>
2023-02-05 21:23:21 +0200

    TD-1 Added solution structure

---
+++ src/ToDoList.Api/Controllers/ToDoController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoList.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ToDoController : ControllerBase
+    {
+
+    }  
+}
