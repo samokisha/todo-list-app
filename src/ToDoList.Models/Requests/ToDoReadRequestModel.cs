7bc912ae2dda16790305a94001416ce93aca451b
eugene <sichi331@gmail.com>
2023-02-06 10:14:28 +0200

    TD-2 Added DTO models & CRUD endpoints

---
+++ src/ToDoList.Models/Requests/ToDoReadRequestModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Models.Requests
+{
+    public class ToDoReadRequestModel
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ src/ToDoList.Models/Responses/ToDoItemResponseModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Models.Responses
+{
+    internal class ToDoItemResponseModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
