e7efa581a052fdc6b03bab1ad6ab08af1edabeab
eugene <sichi331@gmail.com>
2023-02-06 10:14:28 +0200

    TD-2 Added DTO models & CRUD endpoints

--- src/ToDoList.Api/Controllers/ToDoController.cs
+++ src/ToDoList.Api/Controllers/ToDoController.cs
@@ -1,11 +1,36 @@
 using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models.Requests;
 
 namespace ToDoList.Api.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class ToDoController : ControllerBase
     {
+        [HttpPost]
+        [Route("items")]
+        public IActionResult Post([FromBody] ToDoUpdateRequestModel updateRequestModel)
+        {
+            return (IActionResult)updateRequestModel;
+        }
+        [HttpGet]
+        [Route("items")]
+        public IActionResult Get([FromQuery] ToDoReadRequestModel readRequestModel)
+        {
+            return (IActionResult)readRequestModel;
+        }
+        [HttpPut]
+        [Route("items")]
+        public IActionResult Put([FromBody] ToDoCreateRequestModel createRequestModel)
+        {
+            return (IActionResult)createRequestModel;
+        }
+        [HttpDelete]
+        [Route("items")]
+        public IActionResult Delete([FromBody] ToDoDeleteRequestModel deleteRequestModel)
+        {
+            return (IActionResult)deleteRequestModel;
+        }
 
-    }  
+    }
 }
---
+++ src/ToDoList.Models/Requests/ToDoUpdateRequestModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Models.Requests
+{
+    public class ToDoUpdateRequestModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
